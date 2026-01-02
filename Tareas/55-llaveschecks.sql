3ab17d73beb61f41e304d7b44bd2096993b7ae47
Jairo Revilla <revilla.jairo@gmail.com>
2026-01-02 18:33:20 -0500

    Tarea de llaves check

---
+++ Tareas/55-llaveschecks.sql
@@ -0,0 +1,37 @@
+
+SELECT
+  conname
+FROM
+  pg_constraint
+WHERE
+  conrelid = 'city'::regclass;
+
+-- 1. Crear una llave primaria en city (id)
+
+select * from city;
+
+ALTER TABLE city
+	ADD PRIMARY KEY(id);
+
+
+-- 2. Crear un check en population, para que no soporte negativos
+
+ALTER TABLE city
+	ADD CHECK ( population >= 0 ) ;
+
+
+-- 3. Crear una llave primaria compuesta en "countrylanguage"
+-- los campos a usar como llave compuesta son countrycode y language
+
+select * from countrylanguage;
+ALTER TABLE countrylanguage 
+	ADD PRIMARY KEY (
+		countrycode,
+		language
+	);
+
+-- 4. Crear check en percentage, 
+-- Para que no permita negativos ni números superiores a 100
+
+ALTER TABLE countrylanguage
+	ADD CHECK (percentage >= 0 AND percentage <= 100);
