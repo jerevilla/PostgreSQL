13e0339fc12b23efa1cf940a2de0206865a5ec51
Jairo Revilla <revilla.jairo@gmail.com>
2026-01-08 22:29:25 -0500

    clases 64,65,66 y tarea clase 68

---
+++ Clases/64-continent.sql
@@ -0,0 +1,14 @@
+
+
+select DISTINCT continent from country order by continent asc; 
+
+
+INSERT INTO continent (name)
+	SELECT DISTINCT
+		continent
+	FROM
+		country
+	ORDER BY
+		continent ASC;
+
+select * from continent;
